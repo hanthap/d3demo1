ebcedd1469c25631a09f6cd689c991728c5215df
Hantha <69382830+hanthap@users.noreply.github.com>
2025-12-22 11:19:43 +1100

    Move supabase parms into config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://hfunworrmdfxzdtmbvth.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmdW53b3JybWRmeHpkdG1idnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwOTE1MzUsImV4cCI6MjA4MTY2NzUzNX0.lUOR-J6qY-EyjCQv9tPh83mBzdIRq7pCIzstOP7xktU"
+      
+}
